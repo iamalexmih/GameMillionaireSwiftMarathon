5cd4395e053ffb9bf6623a7b049b2e97806ea743
Dmitry <dimapaull@yandex.ru>
2023-02-08 01:19:11 +0300

    Create pyramid question screen

--- GameMillionaire/PyramidQuestionScreen/PyramidQuestionModel.swift
+++ GameMillionaire/PyramidQuestionScreen/PyramidQuestionModel.swift
@@ -10,7 +10,21 @@ import Foundation
  
 class PyramidQuestionList {
     let pyramidQuestionList: [PyramidQuestionModel] = [
-        PyramidQuestionModel(idQuestion: 1, money: 100)
+        PyramidQuestionModel(idQuestion: 1, money: 100),
+        PyramidQuestionModel(idQuestion: 2, money: 200),
+        PyramidQuestionModel(idQuestion: 3, money: 300),
+        PyramidQuestionModel(idQuestion: 4, money: 500),
+        PyramidQuestionModel(idQuestion: 5, money: 1000),
+        PyramidQuestionModel(idQuestion: 6, money: 2000),
+        PyramidQuestionModel(idQuestion: 7, money: 4000),
+        PyramidQuestionModel(idQuestion: 8, money: 8000),
+        PyramidQuestionModel(idQuestion: 9, money: 16000),
+        PyramidQuestionModel(idQuestion: 10, money: 32000),
+        PyramidQuestionModel(idQuestion: 11, money: 64000),
+        PyramidQuestionModel(idQuestion: 12, money: 125000),
+        PyramidQuestionModel(idQuestion: 13, money: 250000),
+        PyramidQuestionModel(idQuestion: 14, money: 500000),
+        PyramidQuestionModel(idQuestion: 15, money: 1000000),
         
     ]
 }
--- GameMillionaire/PyramidQuestionScreen/PyramidQuestionViewController.swift
+++ GameMillionaire/PyramidQuestionScreen/PyramidQuestionViewController.swift
@@ -8,10 +8,41 @@
 import UIKit
 
 class PyramidQuestionViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionButton15: UIButton!
+    @IBOutlet weak var questionButton14: UIButton!
+    @IBOutlet weak var questionButton13: UIButton!
+    @IBOutlet weak var questionButton12: UIButton!
+    @IBOutlet weak var questionButton11: UIButton!
+    @IBOutlet weak var questionButton10: UIButton!
+    @IBOutlet weak var questionButton9: UIButton!
+    @IBOutlet weak var questionButton8: UIButton!
+    @IBOutlet weak var questionButton7: UIButton!
+    @IBOutlet weak var questionButton6: UIButton!
+    @IBOutlet weak var questionButton5: UIButton!
+    @IBOutlet weak var questionButton4: UIButton!
+    @IBOutlet weak var questionButton3: UIButton!
+    @IBOutlet weak var questionButton2: UIButton!
+    @IBOutlet weak var questionButton1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.questionButton1.layer.cornerRadius = 20
+        self.questionButton2.layer.cornerRadius = 20
+        self.questionButton3.layer.cornerRadius = 20
+        self.questionButton4.layer.cornerRadius = 20
+        self.questionButton5.layer.cornerRadius = 20
+        self.questionButton6.layer.cornerRadius = 20
+        self.questionButton7.layer.cornerRadius = 20
+        self.questionButton8.layer.cornerRadius = 20
+        self.questionButton9.layer.cornerRadius = 20
+        self.questionButton10.layer.cornerRadius = 20
+        self.questionButton11.layer.cornerRadius = 20
+        self.questionButton12.layer.cornerRadius = 20
+        self.questionButton13.layer.cornerRadius = 20
+        self.questionButton14.layer.cornerRadius = 20
+        self.questionButton15.layer.cornerRadius = 20
         // Do any additional setup after loading the view.
     }
 }
