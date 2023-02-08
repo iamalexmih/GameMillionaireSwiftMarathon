66d8deefbd597cf9fee5ea4cf99e27d66737c43d
Edgar Isaev <isaev_kdi@mail.ru>
2023-02-08 22:58:01 +0400

    добавил экшены в стартовом экране, вынес текст правил в отдельную переменную

--- GameMillionaire/StartScreen/StartScreenViewController.swift
+++ GameMillionaire/StartScreen/StartScreenViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class StartScreenViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func getToRules(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func getToStart(_ sender: UIButton) {
+    }
 }
+    
+
+    
+
