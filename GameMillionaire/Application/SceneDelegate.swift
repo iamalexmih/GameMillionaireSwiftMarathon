3f135c2e0e9a355fac3dd0fe958ee1e7b69b9a6a
Alex <alexmihgel@gmail.com>
2023-02-09 11:00:52 +0300

    добавлена функция для получения вопроса с учетом раунда

--- GameMillionaire/Application/SceneDelegate.swift
+++ GameMillionaire/Application/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let viewController = ServiceBuilderScreen.createLoseScreen()
+        let viewController = ServiceBuilderScreen.createStartScreen()
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
