6b0b261a04dd41ca6a8027a716b9c8c1d3bf074a
Alex <alexmihgel@gmail.com>
2023-02-11 20:16:34 +0300

    баг фикс. Массив с вопросами восстанавливается при начать новую игру

--- GameMillionaire/CommonModel/Question.swift
+++ GameMillionaire/CommonModel/Question.swift
@@ -50,8 +50,6 @@ enum DifficultyIssue {
 
 class QuestionData {
     
-    var questionNumber = 0
-    var score = 0
     
 static var questionList: [Question] = [
         Question(textQuestion: "Какая кошка самая большая на планете?",
@@ -174,4 +172,92 @@ static var questionList: [Question] = [
                     return randomHardQuestion!
                 }
     }
+}
+
+
+class QuestionStorage {
+    static let questionList: [Question] = [
+        Question(textQuestion: "Какая кошка самая большая на планете?",
+                 difficultyIssue: .lite,
+                 variantsAnswer: ["Лев", "Тигр", "Гепард", "Барс"],
+                 rightAnswer: "Тигр"),
+        
+        Question(textQuestion: "Биткойн - это ...?",
+                 difficultyIssue: .lite,
+                 variantsAnswer: ["Криптовалюта", "Супервалюта", "Скрипка-валюта", "Скрепка-валюта"],
+                 rightAnswer: "Криптовалюта"),
+        
+        Question(textQuestion: "Какие деревья растут из желудей?",
+                 difficultyIssue: .lite,
+                 variantsAnswer: ["Дуб", "Клен", "Гикори", "Грецкий орех"],
+                 rightAnswer: "Дуб"),
+        
+        Question(textQuestion: "Сколько цветов в радуге?",
+                 difficultyIssue: .lite,
+                 variantsAnswer: ["7", "10", "6", "8"],
+                 rightAnswer: "7"),
+        
+        Question(textQuestion: "Какое самое твердое вещество в нашем теле?",
+                 difficultyIssue: .lite,
+                 variantsAnswer: ["Кости", "Волосы", "Ногти", "Зубы"],
+                 rightAnswer: "Зубы"),
+        
+        /// medium
+        Question(textQuestion: "Дата первого полета человека в космос?",
+                 difficultyIssue: .medium,
+                 variantsAnswer: ["12.04.1961", "19.03.1965", "12.06.1956", "10.09.1973"],
+                 rightAnswer: "12.04.1961"),
+        
+        Question(textQuestion: "самый сильный символ в китайской культуре?",
+                 difficultyIssue: .medium,
+                 variantsAnswer: ["Собака", "Крыса", "Обезьяна", "Дракон"],
+                 rightAnswer: "Дракон"),
+        
+        Question(textQuestion: "Самая длинная река в России?",
+                 difficultyIssue: .medium,
+                 variantsAnswer: ["Енисей", "Лена", "Волга", "Обь"],
+                 rightAnswer: "Лена"),
+        
+        Question(textQuestion: "Как называется длина круга?",
+                 difficultyIssue: .medium,
+                 variantsAnswer: ["Диаметр", "Радиус", "Окружность", "Площадь"],
+                 rightAnswer: "Окружность"),
+
+        
+        Question(textQuestion: "Где самая быстрая мышца в теле?",
+                 difficultyIssue: .medium,
+                 variantsAnswer: ["Нога", "Рука", "Пальцы", "Глаз"],
+                 rightAnswer: "Глаз"),
+        
+        /// hard
+        Question(textQuestion: "Сколько в России субъектов Федерации?",
+                 difficultyIssue: .hard,
+                 
+                 variantsAnswer: ["85", "120", "62", "74"],
+                 rightAnswer: "85"),
+        
+        Question(textQuestion: "Сколько костей в теле взрослого человека?",
+                 difficultyIssue: .hard,
+                 
+                 variantsAnswer: ["501", "105", "206", "347"],
+                 rightAnswer: "206"),
+        
+        Question(textQuestion: "Что изобрели и испытали братья Райт?",
+                 difficultyIssue: .hard,
+                 
+                 variantsAnswer: ["Компьютер", "Автомобиль", "Телефон", "Самолет"],
+                 rightAnswer: "Самолет"),
+        
+        Question(textQuestion: "Кто расписывал потолок Сикстинской капеллы?",
+                 difficultyIssue: .hard,
+                 variantsAnswer: ["Пабло Пикассо", "Леонардо Да Винчи", "Микеланджело", "Винсент Ван Гог"],
+                 rightAnswer: "Микеланджело"),
+        
+        Question(textQuestion: "трехзначное число: вторая цифра в четыре раза больше третьей, первая – на три меньше второй?",
+                 difficultyIssue: .hard,
+                 variantsAnswer: ["282", "693", "141", "241"],
+                 rightAnswer: "141"),
+    ]
+    
+    
 }
